f0a511a8dee9af1575aac165310b58761806ca76
rajput-hemant <rajput.hemant2001@gmail.com>
2023-05-20 21:11:47 +0530

    add tests and search logic
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, error::Error, fs, println, process};
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,34 +13,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In File {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
-    }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
